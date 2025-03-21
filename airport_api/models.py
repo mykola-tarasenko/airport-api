072ec5f3feac1ade14bc8a15440ac9446d045a0a
mykola <nikolajtarasenko71@gmail.com>
2025-03-21 12:16:41 +0200

    Add AirplaneType model

--- airport_api/models.py
+++ airport_api/models.py
@@ -87,3 +87,13 @@ class Route(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
