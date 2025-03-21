61ce0a0a8f0cf4ae3b1cb2ab19a00c39a9b68385
mykola <nikolajtarasenko71@gmail.com>
2025-03-21 12:14:01 +0200

    Add Route Model

--- airport_api/models.py
+++ airport_api/models.py
@@ -1,4 +1,7 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import UniqueConstraint
 
 
 class City(models.Model):
@@ -25,3 +28,62 @@ class Airport(models.Model):
 
     def __str__(self):
         return f"{self.city.name} - {self.name}"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="departing_routs",
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="arriving_routs",
+    )
+    distance = models.IntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        constraints = (
+            UniqueConstraint(
+                fields=["source", "destination"],
+                name="unique_route",
+            ),
+        )
+        ordering = ("source", "destination")
+
+    @staticmethod
+    def validate_source_and_destination(
+            source,
+            destination,
+            error_to_raise,
+    ):
+        if source == destination:
+            raise error_to_raise("The source can`t equal destination.")
+
+    def clean(self) -> None:
+        Route.validate_source_and_destination(
+            self.source,
+            self.destination,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        *args,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Route, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    @property
+    def name(self):
+        return f"{self.source.city.name} - {self.destination.city.name}"
+
+    def __str__(self):
+        return self.name
