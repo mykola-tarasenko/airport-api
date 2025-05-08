2cb71dc441d33cedd0321fd3b2d507084d0fc355
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-05-08 18:15:26 +0300

    Add AirportTest

--- airport_api/tests/factories.py
+++ airport_api/tests/factories.py
@@ -1,4 +1,8 @@
-from airport_api.models import City
+from airport_api.models import (
+    City,
+    Airport,
+    Route,
+)
 
 
 def sample_city(as_dict=False, **params):
@@ -8,3 +12,12 @@ def sample_city(as_dict=False, **params):
     }
     defaults.update(params)
     return defaults if as_dict else City.objects.create(**defaults)
+
+
+def sample_airport(as_dict=False, **params):
+    defaults = {
+        "name": "Holovnyi",
+        "city": sample_city(),
+    }
+    defaults.update(params)
+    return defaults if as_dict else Airport.objects.create(**defaults)
--- airport_api/tests/test_models.py
+++ airport_api/tests/test_models.py
@@ -1,9 +1,20 @@
 from django.test import TestCase
 
-from airport_api.tests.factories import sample_city
+from airport_api.tests.factories import (
+    sample_city,
+    sample_airport,
+)
 
 
 class CityTest(TestCase):
     def test_str_method(self):
         city =  sample_city()
         self.assertEqual(str(city), city.name)
+
+
+class AirportTest(TestCase):
+    def test_str_method(self):
+        airport = sample_airport()
+        self.assertEqual(
+            str(airport), f"{airport.city.name} - {airport.name}"
+        )
